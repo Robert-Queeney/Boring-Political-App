82c8669399c0425f4405a80cc9d432007dbcb4ce
scoslo5512 <32365387+scoslo5512@users.noreply.github.com>
2018-03-31 08:17:50 -0700

    Update sample.js
    Added ajax call for bills

--- sample.js
+++ sample.js
@@ -8,3 +8,21 @@ $.ajax({
   }).done(function(data){
   console.log(data)
   });
+
+// searching for upcoming bills
+// search terms
+// "https://api.propublica.org/congress/v1/bills/search.json?query=${input}"
+
+  $.ajax({
+    url: "https://api.propublica.org/congress/v1/bills/search.json?query=school",
+    type: "GET",
+    dataType: 'json',
+    headers: {'X-API-Key': 'um0ROEiltrFHkDwAqWjHR1es1j2wmaz8KekzLuDZ'}
+  }).done(function(data){
+  console.log(data)
+  });
+
+  // append info on bills to new element on second page
+  // Bill name, voting date, summary
+
+  //   https://projects.propublica.org/api-docs/congress-api/bills/#search-bills
