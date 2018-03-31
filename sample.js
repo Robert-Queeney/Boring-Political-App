4630c734a838580758caf14fe6465c46d3bfe4aa
scoslo5512 <robert.queeney@gmail.com>
2018-03-31 09:46:26 -0700

    created billInfo object from ajax call

--- sample.js
+++ sample.js
@@ -1,26 +1,45 @@
 
 
-$.ajax({
-    url: "https://api.propublica.org/congress/v1/members/senate/MN/current.json",
-    type: "GET",
-    dataType: 'json',
-    headers: {'X-API-Key': 'um0ROEiltrFHkDwAqWjHR1es1j2wmaz8KekzLuDZ'}
-  }).done(function(data){
-  console.log(data)
-  });
+// $.ajax({
+//     url: "https://api.propublica.org/congress/v1/members/senate/MN/current.json",
+//     type: "GET",
+//     dataType: 'json',
+//     headers: {'X-API-Key': 'um0ROEiltrFHkDwAqWjHR1es1j2wmaz8KekzLuDZ'}
+//   }).done(function(data){
+//   // console.log(data)
+//   });
 
 // searching for upcoming bills
 // search terms
 // "https://api.propublica.org/congress/v1/bills/search.json?query=${input}"
 
   $.ajax({
-    url: "https://api.propublica.org/congress/v1/bills/search.json?query=school",
+    url: "https://api.propublica.org/congress/v1/bills/search.json?query=taxes",
     type: "GET",
     dataType: 'json',
     headers: {'X-API-Key': 'um0ROEiltrFHkDwAqWjHR1es1j2wmaz8KekzLuDZ'}
-  }).done(function(data){
-  console.log(data)
-  });
+  }).then(function(results){
+    console.log(results); 
+    for(let i = 0 ; i  < results.results[0].bills.length; i++ ){
+      // creating const to use bill data for second page
+      let title = results.results[0].bills[i].short_title; 
+      let id = results.results[0].bills[i].bill_id; 
+      let party = results.results[0].bills[i].sponsor_party;  
+      let summary = results.results[0].bills.title; 
+      let status = results.results[0].bills.latest_major_action; 
+
+    
+      const billInfo = {
+        title: title, 
+        id: id, 
+        party: party, 
+        summary: summary, 
+        status: status
+      }
+
+     console.log("bill-info", billInfo); 
+  
+  }});
 
   // append info on bills to new element on second page
   // Bill name, voting date, summary
