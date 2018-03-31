8d0d7f05234d30af0dbf144ca4ac1e7c237772c0
Danielthweatt <dthweatt192@yahoo.com>
2018-03-31 13:41:00 -0500

    working with Google Civic API calls

--- sample.js
+++ sample.js
@@ -1,33 +1,41 @@
 'use strict';
 
-$.ajax({
-  url: "https://api.propublica.org/congress/v1/members/senate/MN/current.json",
-  type: "GET",
-  dataType: 'json',
-  headers: {'X-API-Key': 'um0ROEiltrFHkDwAqWjHR1es1j2wmaz8KekzLuDZ'}
-}).done(function(data){
-  console.log(data)
-});
+// Variables
+
+let address;
+
+// $.ajax({
+//   url: "https://api.propublica.org/congress/v1/members/senate/MN/current.json",
+//   type: "GET",
+//   dataType: 'json',
+//   headers: {'X-API-Key': 'um0ROEiltrFHkDwAqWjHR1es1j2wmaz8KekzLuDZ'}
+// }).done(function(data){
+//   console.log(data)
+// });
+
+address = '3416 W. 87th St., Bloomington, MN';
 
 $.ajax({
-  url: "https://www.googleapis.com/civicinfo/v2/representatives?key=AIzaSyC5mPRvRl9aDc6c0fbeQVooykzgH6CaIQU&address=1890 Buford Ave., St. Paul, MN",
-  method: "GET"
+  url: `https://www.googleapis.com/civicinfo/v2/representatives?key=AIzaSyC5mPRvRl9aDc6c0fbeQVooykzgH6CaIQU&address=${address}&levels=country&roles=`,
+  method: 'GET'
 }).then(function(response){
   console.log(response)
 });
+
+
 
 // searching for upcoming bills
 // search terms
 // "https://api.propublica.org/congress/v1/bills/search.json?query=${input}"
 
-$.ajax({
-  url: "https://api.propublica.org/congress/v1/bills/search.json?query=school",
-  type: "GET",
-  dataType: 'json',
-  headers: {'X-API-Key': 'um0ROEiltrFHkDwAqWjHR1es1j2wmaz8KekzLuDZ'}
-}).done(function(data){
-  console.log(data)
-});
+// $.ajax({
+//   url: "https://api.propublica.org/congress/v1/bills/search.json?query=school",
+//   type: "GET",
+//   dataType: 'json',
+//   headers: {'X-API-Key': 'um0ROEiltrFHkDwAqWjHR1es1j2wmaz8KekzLuDZ'}
+// }).done(function(data){
+//   console.log(data)
+// });
 
 // append info on bills to new element on second page
 // Bill name, voting date, summary
