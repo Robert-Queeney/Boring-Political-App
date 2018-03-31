e7d2598edb3431fbb1209823a834afcc7a4aeeab
Danielthweatt <dthweatt192@yahoo.com>
2018-03-31 11:46:42 -0500

    working with Google Civic API

--- sample.js
+++ sample.js
@@ -1,35 +1,35 @@
-
+'use strict';
 
 $.ajax({
-    url: "https://api.propublica.org/congress/v1/members/senate/MN/current.json",
-    type: "GET",
-    dataType: 'json',
-    headers: {'X-API-Key': 'um0ROEiltrFHkDwAqWjHR1es1j2wmaz8KekzLuDZ'}
-  }).done(function(data){
+  url: "https://api.propublica.org/congress/v1/members/senate/MN/current.json",
+  type: "GET",
+  dataType: 'json',
+  headers: {'X-API-Key': 'um0ROEiltrFHkDwAqWjHR1es1j2wmaz8KekzLuDZ'}
+}).done(function(data){
   console.log(data)
-  });
+});
 
-  $.ajax({
-    url: "https://www.googleapis.com/civicinfo/v2/representatives?key=AIzaSyC5mPRvRl9aDc6c0fbeQVooykzgH6CaIQU&address=1263%20Pacific%20Ave.%20Kansas%20City%20KS",
-    method: "GET"
-  }).then(function(response){
+$.ajax({
+  url: "https://www.googleapis.com/civicinfo/v2/representatives?key=AIzaSyC5mPRvRl9aDc6c0fbeQVooykzgH6CaIQU&address=1890 Buford Ave., St. Paul, MN",
+  method: "GET"
+}).then(function(response){
   console.log(response)
-  });
-  
+});
+
 // searching for upcoming bills
 // search terms
 // "https://api.propublica.org/congress/v1/bills/search.json?query=${input}"
 
-  $.ajax({
-    url: "https://api.propublica.org/congress/v1/bills/search.json?query=school",
-    type: "GET",
-    dataType: 'json',
-    headers: {'X-API-Key': 'um0ROEiltrFHkDwAqWjHR1es1j2wmaz8KekzLuDZ'}
-  }).done(function(data){
+$.ajax({
+  url: "https://api.propublica.org/congress/v1/bills/search.json?query=school",
+  type: "GET",
+  dataType: 'json',
+  headers: {'X-API-Key': 'um0ROEiltrFHkDwAqWjHR1es1j2wmaz8KekzLuDZ'}
+}).done(function(data){
   console.log(data)
-  });
+});
 
-  // append info on bills to new element on second page
-  // Bill name, voting date, summary
+// append info on bills to new element on second page
+// Bill name, voting date, summary
 
-  //   https://projects.propublica.org/api-docs/congress-api/bills/#search-bills
+//   https://projects.propublica.org/api-docs/congress-api/bills/#search-bills
