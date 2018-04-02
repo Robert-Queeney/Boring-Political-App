0e53a7ee204c94bf471f6f47f0a84b2ab28b2757
Cavan Wagg <waggx002@gmail.com>
2018-04-01 21:40:20 -0500

    added interact.js, troubleshooting drag n drop feature

--- sample.js
+++ sample.js
@@ -1,5 +1,77 @@
-'use strict';
+// 'use strict';
+//Interact js from npm
+
+
 $(document).ready(function(){
+ 
+
+
+// target elements with the "draggable" class
+interact('.draggable')
+  .draggable({
+    // enable inertial throwing
+    inertia: true,
+    // keep the element within the area of it's parent
+    // restrict: {
+    //   restriction: "parent",
+    //   endOnly: true,
+    //   elementRect: { top: 0, left: 0, bottom: 1, right: 1 }
+    // },
+    // enable autoScroll
+    autoScroll: true,
+    // call this function on every dragmove event
+    onmove: dragMoveListener,
+  });
+
+  function dragMoveListener (event) {
+    var target = event.target,
+        // keep the dragged position in the data-x/data-y attributes
+        x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
+        y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
+
+    // translate the element
+    target.style.webkitTransform =
+    target.style.transform =
+      'translate(' + x + 'px, ' + y + 'px)';
+
+    // update the posiion attributes
+    target.setAttribute('data-x', x);
+    target.setAttribute('data-y', y);
+  }
+
+  // enable draggables to be dropped into this
+interact('.dropzone').dropzone({
+  // Require a 50% element overlap for a drop to be possible
+  overlap: 0.50,
+
+  // listen for drop related events:
+
+  ondropactivate: function (event) {
+    // add active dropzone feedback
+    event.target.classList.add('drop-active');
+  },
+  ondragenter: function (event) {
+    var draggableElement = event.relatedTarget,
+        dropzoneElement = event.target;
+
+    // feedback the possibility of a drop
+    dropzoneElement.classList.add('drop-target');
+  },
+  ondragleave: function (event) {
+    // remove the drop feedback style
+    event.target.classList.remove('drop-target');
+  },
+  ondrop: function (event) {
+    event.relatedTarget.textContent = 'Dropped';
+  },
+  ondropdeactivate: function (event) {
+    // remove active dropzone feedback
+    event.target.classList.remove('drop-active');
+    event.target.classList.remove('drop-target');
+  }
+});
+
+  
 
   // Initialize Firebase
 //   var config = {
@@ -44,7 +116,7 @@ $(document).ready(function(){
     dataType: 'json',
     headers: {'X-API-Key': 'um0ROEiltrFHkDwAqWjHR1es1j2wmaz8KekzLuDZ'}
   }).then(function(results){
-    console.log(results); 
+    console.log("results====>", results); 
     for(let i = 0 ; i  < results.results[0].bills.length; i++ ){
       // creating const to use bill data for second page
       let title = results.results[0].bills[i].short_title; 
